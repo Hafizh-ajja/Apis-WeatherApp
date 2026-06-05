d1d0cf71e55159858bc02d151f70048b3dd132ea
Hafizh-ajja <hhafizh662@email.com>
2026-06-05 08:18:39 +0700

    update-animationLoading

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,19 +1,71 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from '../pages/Home'
 import Forecast from '../pages/Forecast'
+import { getWeatherByCoords } from '../utils/weatherApi'
 
 export default function Router() {
-  const [city, setCity] = useState('Jakarta')
+  const [city, setCity] = useState('')
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      async (position) => {
+        try {
+          const data = await getWeatherByCoords(
+            position.coords.latitude,
+            position.coords.longitude
+          )
+
+          setCity(data.name)
+        } catch (error) {
+          console.error(error)
+          setCity('Jakarta')
+        }
+      },
+      (error) => {
+        console.error(error)
+        setCity('Jakarta')
+      }
+    )
+  }, [])
+
+  if (!city) {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black">
+      <div className="text-center text-white">
+
+        <div className="text-7xl animate-bounce">
+          🌤️
+        </div>
+
+        <h2 className="text-2xl font-semibold mt-4">
+          WeatherPiss
+        </h2>
+
+        <p className="text-white/70 mt-2">
+          Mengambil lokasi Anda...
+        </p>
+
+      </div>
+    </div>
+  )
+}
 
   return (
     <BrowserRouter>
-   
       <Routes>
-        <Route path="/" element={<Home city={city} setCity={setCity} />} />
-        <Route path="/forecast" element={<Forecast city={city} />} />
+        <Route
+          path="/"
+          element={<Home city={city} setCity={setCity} />}
+        />
+
+        <Route
+          path="/forecast"
+          element={<Forecast city={city} />}
+        />
       </Routes>
     </BrowserRouter>
   )
 }
+
